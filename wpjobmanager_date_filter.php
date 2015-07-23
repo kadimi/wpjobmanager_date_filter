e38b75d43f3dd49c486775da2962d56fc16bd0c3
Nabil Kadimi <nabil@kadimi.com>
2015-07-23 12:22:46 +0100

    Initialize repository

---
+++ wpjobmanager_date_filter.php
@@ -0,0 +1,11 @@
+<?php 
+
+/*
+Plugin Name: Date Filter for WP Job Manager
+Plugin URI: http://www.kadimi.com/
+Description: -
+Version: 1.0.0
+Author: Nabil Kadimi
+Author URI: http://kadimi.com
+License: GPL2
+*/
