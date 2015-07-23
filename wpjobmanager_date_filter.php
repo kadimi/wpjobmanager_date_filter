b85559f47e84168568738aa9e4a3ab0698c430be
Nabil Kadimi <nabil@kadimi.com>
2015-07-23 17:24:00 +0100

    Setup date filters

--- wpjobmanager_date_filter.php
+++ wpjobmanager_date_filter.php
@@ -11,6 +11,7 @@ License: GPL2
 */
 
 add_action( 'job_manager_job_filters_search_jobs_end', 'wpjobmanager_date_filter_fields' );
+add_filter( 'job_manager_get_listings', 'filter_by_date_query_args', 10, 2 );
 
 /**
  * Outputs 'from' and 'to' fields HTML
@@ -19,11 +20,76 @@ function wpjobmanager_date_filter_fields () {
 	?><div class="search_date" style="margin-top: 20px">
 		<div class="search_date_from_wrapper" style="float: left; padding-right: 0.5em; width: 50%;">
 			<label for="search_date_from">Date from</label>
-			<input type="text" name="search_date_from" id="search_date_from" placeholder="dd/mm/yyyy" />
+			<input type="text" name="search_date_from" id="search_date_from" placeholder="mm/dd/yyyy" />
 		</div>
 		<div class="search_date_to_wrapper" style="float: right; padding-left: 0.5em; width: 50%;">
-			<label for="search_keywords">Date to</label>
-			<input type="text" name="search_date_to" id="search_date_to" placeholder="dd/mm/yyyy" />
+			<label for="search_date_to">Date to</label>
+			<input type="text" name="search_date_to" id="search_date_to" placeholder="mm/dd/yyyy" />
 		</div>
-	</div><?php
+	</div>
+
+​	<script>
+		jQuery( document ).ready( function( $ ) {
+
+			$( '#search_date_from,#search_date_to' )
+				.change( function() { $('#search_keywords').change(); } )
+				.keyup( function( e ) {
+					13 === e.which && $(this).trigger("change")
+				} )
+			;
+		} );
+	</script>
+
+	<?php
+}
+
+/**
+ * Adds date_query to $query_args
+ */
+function filter_by_date_query_args( $query_args, $args ) {
+	if ( isset( $_POST['form_data'] ) ) {
+
+		parse_str( $_POST['form_data'], $form_data );
+
+		if ( ! empty( $form_data[ 'search_date_from' ] ) && trim( $form_data[ 'search_date_from' ] ) ) {
+
+			preg_match( '/^(?<month>\d{1,2})\/(?<day>\d{1,2})\/(?<year>\d{4})$/'
+				, $form_data[ 'search_date_from' ]
+				, $date
+			);
+
+			if ( $date ) {
+
+				$query_args[ 'date_query' ]['after'] = array(
+					'month' => $date[ 'month' ],
+					'day'   => $date[ 'day' ],
+					'year'  => $date[ 'year' ],
+				);
+			}
+
+			// Show the 'reset' link
+			add_filter( 'job_manager_get_listings_custom_filter', '__return_true' );
+		}
+
+		if ( ! empty( $form_data[ 'search_date_to' ] ) && trim( $form_data[ 'search_date_to' ] ) ) {
+
+			preg_match( '/^(?<month>\d{1,2})\/(?<day>\d{1,2})\/(?<year>\d{4})$/'
+				, $form_data[ 'search_date_to' ]
+				, $date
+			);
+
+			if ( $date ) {
+
+				$query_args[ 'date_query' ]['after'] = array(
+					'month' => $date[ 'month' ],
+					'day'   => $date[ 'day' ],
+					'year'  => $date[ 'year' ],
+				);
+			}
+
+			// Show the 'reset' link
+			add_filter( 'job_manager_get_listings_custom_filter', '__return_true' );
+		}
+	}
+	return $query_args;
 }
